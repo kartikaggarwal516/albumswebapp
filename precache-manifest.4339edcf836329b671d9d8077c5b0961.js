713d397e0acf52b0858d28dfa452db3055e2cdb2
Kartik <aggarwalkartik96@gmail.com>
2020-09-15 12:03:55 +0530

    Updates

---
+++ precache-manifest.4339edcf836329b671d9d8077c5b0961.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a941037548376cfdd3cbed8d346d6170",
+    "url": "/albumswebapp/index.html"
+  },
+  {
+    "revision": "7a302500fdf2d079bd88",
+    "url": "/albumswebapp/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "f72873eaa44a5486a56b",
+    "url": "/albumswebapp/static/css/main.acac6592.chunk.css"
+  },
+  {
+    "revision": "7a302500fdf2d079bd88",
+    "url": "/albumswebapp/static/js/2.5c0ebe20.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/albumswebapp/static/js/2.5c0ebe20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f72873eaa44a5486a56b",
+    "url": "/albumswebapp/static/js/main.75be4bb7.chunk.js"
+  },
+  {
+    "revision": "64adfd2c3d040d131924",
+    "url": "/albumswebapp/static/js/runtime-main.cc9be27c.js"
+  }
+]);
